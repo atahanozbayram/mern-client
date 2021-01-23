f1309461e4a19c481947a4bd60232f6ed0c1927e
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-01-23 04:11:02 +0300

    Change export to named import to keep code consistent

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import LoginPage from '@root/pages/login';
 
 const App = function () {
-	return <h1>Testing</h1>;
+	return (
+		<Router>
+			<Switch>
+				<Route path="/login">
+					<LoginPage />
+				</Route>
+				<Route path="/register"></Route>
+				<Route path="/todo"></Route>
+				<Route path="/"></Route>
+			</Switch>
+		</Router>
+	);
 };
 
-export { App as default };
+export { App };
