6396b4dbde1ba32907cbeded18766d598f5af3f1
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-03 15:40:04 +0300

    Add login request function

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { PageHeader } from '@components/PageHeader';
+import axios from 'axios';
 
 const LoginForm = function (props) {
+	const emailInput = useRef(null);
+	const passwordInput = useRef(null);
+	const [errorArr, setErrorArr] = useState([]);
+
+	const loginRequestFunc = function (event) {
+		event.preventDefault();
+		// check if the references are not null
+		// if they are terminate the function
+
+		if (emailInput === null || passwordInput === null) return;
+
+		const host = process.env.BACKEND_HOST || 'http://localhost';
+		const port = process.env.BACKEND_PORT || 3000;
+
+		axios({
+			method: 'POST',
+			baseURL: `${host}:${port}/api`,
+			url: '/user/login',
+			data: {
+				email: emailInput.current.value,
+				password: passwordInput.current.value,
+			},
+			withCredentials: true,
+			maxRedirects: 0,
+		})
+			.then((res) => {
+				// set errorArr to empty array
+				setErrorArr([]);
+			})
+			.catch((err) => {
+				console.log('err.response: %o', err.response.data);
+				setErrorArr(err.response.data.errors);
+			});
+	};
+
 	return (
 		<div>
 			<PageHeader title="Login" />
 			<div>
+				<div>
+					{errorArr.map((value, index) => {
+						return <div key={index}>{value.msg}</div>;
+					})}
+				</div>
 				<form>
 					<div>
 						<div>
 							<label>E-mail:</label>
 						</div>
-						<input type="text" name="email" autoComplete="off" />
+						<input type="text" name="email" autoComplete="off" ref={emailInput} />
 					</div>
 					<div>
 						<div>
 							<label>Password:</label>
 						</div>
-						<input type="password" name="password" autoComplete="off" />
+						<input type="password" name="password" autoComplete="off" ref={passwordInput} />
 					</div>
 					<div>
-						<button>Login</button>
+						<button onClick={loginRequestFunc}>Login</button>
 					</div>
 				</form>
 			</div>
