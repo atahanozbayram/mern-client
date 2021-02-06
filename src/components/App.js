a9fd0a2243087cd601783e06d5c351de63bb18e5
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-06 22:25:56 +0300

    Add PrivateRoute to hide todo path

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { LoginPage } from '@root/pages/login';
+import { PrivateRoute } from '@components/PrivateRoute';
 
 const App = function () {
 	return (
@@ -10,7 +11,9 @@ const App = function () {
 					<LoginPage />
 				</Route>
 				<Route path="/register"></Route>
-				<Route path="/todo"></Route>
+				<PrivateRoute>
+					<Route path="/todo"></Route>
+				</PrivateRoute>
 				<Route path="/"></Route>
 			</Switch>
 		</Router>
