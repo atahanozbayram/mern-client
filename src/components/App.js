daeb6548aaf59292b8f0e36edc7359d8b21665c5
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-13 03:16:27 +0300

    Add TodoPage into rendering and change some imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { LoginPage } from '@root/pages/login';
+import { LoginPage } from '@pages/login';
+import { TodoPage } from '@pages/todo';
 import { PrivateRoute } from '@components/PrivateRoute';
 
 const App = function () {
@@ -12,7 +13,9 @@ const App = function () {
 				</Route>
 				<Route path="/register"></Route>
 				<PrivateRoute>
-					<Route path="/todo"></Route>
+					<Route path="/todo">
+						<TodoPage />
+					</Route>
 				</PrivateRoute>
 				<Route path="/"></Route>
 			</Switch>
