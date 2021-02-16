e726197b223e8aa6acdab23668967c0f387a6d1a
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-16 04:48:36 +0300

    Refactor code

--- src/components/CustomRoute.js
+++ src/components/CustomRoute.js
@@ -1,10 +1,16 @@
 import React, { Children, useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import cookie from 'js-cookie';
+import { isLoggedIn } from '@utils/log-status';
 
 const CustomRoute = function (props) {
-	if (cookie.get('refreshToken') === undefined) {
-		return <Redirect to={{ pathname: '/login' }} />;
+	const { routeType, redirectPath } = props;
+
+	let currentStatus;
+	if (isLoggedIn() === true) currentStatus = 'private';
+	else currentStatus = 'public';
+
+	if (routeType !== currentStatus) {
+		return <Redirect to={{ pathname: redirectPath }} />;
 	}
 
 	return props.children;
