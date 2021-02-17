8212749ade572b3211439d8ffaee4dd3f4007e96
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-18 02:34:40 +0300

    Add functions to log in and log out

--- src/utils/log-status.js
+++ src/utils/log-status.js
@@ -1,10 +1,46 @@
-import cookie from 'js-cookie';
+import jscookie from 'js-cookie';
+import { axiosInstance } from '@utils/axios-instance';
 
 const isLoggedIn = function () {
-	if (cookie.get('refreshToken') === undefined) {
+	if (jscookie.get('refreshToken') === undefined) {
 		return false;
 	}
 	return true;
 };
 
-export { isLoggedIn };
+const logIn = function (email, password) {
+	return new Promise((resolve, reject) => {
+		axiosInstance({
+			method: 'POST',
+			url: '/user/login',
+			maxRedirects: 0,
+			data: {
+				email: email,
+				password: password,
+			},
+		})
+			.then((res) => {
+				console.log('logIn axios then'); // TODO: delete this logging
+				const { firstName, lastName, email } = res.data.user;
+
+				localStorage.setItem('firstName', firstName);
+				localStorage.setItem('lastName', lastName);
+				localStorage.setItem('email', email);
+				resolve(res);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
+
+const logOut = function () {
+	localStorage.removeItem('firstName');
+	localStorage.removeItem('lastName');
+	localStorage.removeItem('email');
+
+	// clean up the refreshToken cookie
+	jscookie.remove('refreshToken');
+};
+
+export { isLoggedIn, logIn, logOut };
