19adb74b93781e96d14269e802a8fadbaf44ae48
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2021-02-17 05:29:05 +0300

    Add register request functionality

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,64 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PageHeader } from '@components/PageHeader';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import { axiosInstance } from '@utils/axios-instance';
 
 const RegisterPage = function (props) {
+	const [formInfo, setFormInfo] = useState({});
+	const [redirect, setRedirect] = useState(false);
+	const [errorArr, setErrorArray] = useState([]);
+
+	const handleChange = function (event) {
+		setFormInfo((currentState) => {
+			return Object.assign({}, currentState, { [event.target.name]: event.target.value });
+		});
+	};
+
+	const registerRequestFunc = function (event) {
+		event.preventDefault();
+		setErrorArray([]);
+		let validationErr = false;
+
+		// match email, emailConfirmation and password, passwordConfirmation
+		if (formInfo.email !== formInfo.emailConfirmation) {
+			setErrorArray((currentErrorArr) => {
+				return [...currentErrorArr, { msg: 'Email and email confirmation must be same.' }];
+			});
+
+			validationErr = true;
+		}
+
+		if (formInfo.password !== formInfo.passwordConfirmation) {
+			setErrorArray((currentErrorArr) => {
+				return [...currentErrorArr, { msg: 'Password and password confirmation must be same.' }];
+			});
+
+			validationErr = true;
+		}
+
+		if (validationErr) return;
+
+		axiosInstance({
+			method: 'POST',
+			url: '/user/register',
+			data: formInfo,
+		})
+			.then((res) => {
+				setRedirect(true);
+			})
+			.catch((err) => {
+				console.error(err);
+				setRedirect(false);
+				setErrorArray(err.response.data.errors);
+			});
+	};
+
 	return (
 		<div>
-			<form>
+			<PageHeader title="Register" />
+			<div>
 				<div>
-					<label>First name:</label>
+					{errorArr.map((value, index) => {
+						return <div key={index}>{value.msg}</div>;
+					})}
 				</div>
-				<div>
-					<input type="text" name="firstName" autoComplete="off" />
-				</div>
-				<div>
+				<form>
 					<div>
-						<label>Last name:</label>
+						<label>First name:</label>
 					</div>
 					<div>
-						<input type="text" name="lastName" autoComplete="off" />
-					</div>
-				</div>
-				<div>
-					<div>
-						<label>E-mail:</label>
+						<input type="text" name="firstName" autoComplete="off" onChange={handleChange} />
 					</div>
 					<div>
-						<input type="text" name="email" autoComplete="off" />
-					</div>
-				</div>
-				<div>
-					<div>
-						<label>E-mail confirmation:</label>
-					</div>
-					<div>
-						<input type="text" name="emailConfirmation" autoComplete="off" />
-					</div>
-				</div>
-				<div>
-					<div>
-						<label>Password:</label>
+						<div>
+							<label>Last name:</label>
+						</div>
+						<div>
+							<input type="text" name="lastName" autoComplete="off" onChange={handleChange} />
+						</div>
 					</div>
 					<div>
-						<input type="password" name="password" autoComplete="off" />
+						<div>
+							<label>E-mail:</label>
+						</div>
+						<div>
+							<input type="text" name="email" autoComplete="off" onChange={handleChange} />
+						</div>
 					</div>
-				</div>
-				<div>
 					<div>
-						<label>Password confirmation:</label>
+						<div>
+							<label>E-mail confirmation:</label>
+						</div>
+						<div>
+							<input type="text" name="emailConfirmation" autoComplete="off" onChange={handleChange} />
+						</div>
 					</div>
 					<div>
-						<input type="password" name="passwordConfirmation" autoComplete="off" />
+						<div>
+							<label>Password:</label>
+						</div>
+						<div>
+							<input type="password" name="password" autoComplete="off" onChange={handleChange} />
+						</div>
 					</div>
-				</div>
-				<div>
 					<div>
-						<button>Register</button>
+						<div>
+							<label>Password confirmation:</label>
+						</div>
+						<div>
+							<input type="password" name="passwordConfirmation" autoComplete="off" onChange={handleChange} />
+						</div>
 					</div>
-				</div>
-			</form>
+					<div>
+						<div>
+							<button onClick={registerRequestFunc}>Register</button>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
