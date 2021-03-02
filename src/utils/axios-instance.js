06422cb16c54a617ee22d84bfa8688fac9215164
Atahan Ozbayram <atahan.ozbayram@hotmail.com>
2021-03-02 10:19:19 +0300

    Change usage of env variables

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const backend_url = process.env.BACKEND_HOST || 'http://localhost:3000';
+console.log('backend_url: ', backend_url);
 
 const axiosInstance = axios.create({
 	baseURL: `${backend_url}/api`,
