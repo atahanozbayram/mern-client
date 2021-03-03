c60b3aa306794ca3423e1bff7804e38c5dd58d25
Atahan Ozbayram <atahan.ozbayram@hotmail.com>
2021-03-03 09:58:16 +0300

    Add new webpack configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,11 +4,17 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const { merge } = require('webpack-merge');
 const { commonConfig } = require('./webpack.common');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const prodConfig = merge(commonConfig, {
 	name: 'prod',
 	entry: '/src/index.js',
 	mode: 'production',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
 });
 
 module.exports = prodConfig;
